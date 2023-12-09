0e51b279c027a46d8ca5b47066dee02101bd5dd7
jose <joseakd136@gmail.com>
2023-12-08 23:50:40 -0300

    Login funcional, firebase conectado para el login

--- app/src/main/java/com/example/for2pay/LoginActivity.java
+++ app/src/main/java/com/example/for2pay/LoginActivity.java
@@ -1,12 +1,21 @@
 package com.example.for2pay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     Button lgnButton;
     Button regButton;
 
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +36,40 @@ public class LoginActivity extends AppCompatActivity {
         lgnButton=findViewById(R.id.btn_login);
         regButton=findViewById(R.id.btn_registrarse);
 
+        mAuth=FirebaseAuth.getInstance();
+
         lgnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(correo.getText().toString().equals("admin") && contrasenia.getText().toString().equals("123")){
-                    Toast.makeText(LoginActivity.this, "Login exitoso", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(LoginActivity.this, "Login erroneo", Toast.LENGTH_SHORT).show();
+                String emailUser = correo.getText().toString().trim();
+                String pwdUser =contrasenia.getText().toString().trim();
+                if (emailUser.isEmpty() || pwdUser.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Complete los campos", Toast.LENGTH_SHORT).show();
+                } else{
+                    fncInicioSesion(emailUser, pwdUser);
                 }
             }
         });
-
     }
+
+    private void fncInicioSesion(String p_email, String p_pwd){
+        mAuth.signInWithEmailAndPassword(p_email, p_pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    finish();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    Toast.makeText(LoginActivity.this, "Inicio sesion con exito", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Error al iniciar sesion", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
